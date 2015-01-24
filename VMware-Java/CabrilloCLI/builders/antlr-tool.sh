9099c20a14679c3708d4f22568cc31504fe7c0ac
Mike Matera <matera@lifealgorithmic.com>
2015-01-24 09:05:10 -0800

    Relative paths in ANTLR builder

--- VMware-Java/CabrilloCLI/builders/antlr-tool.sh
+++ VMware-Java/CabrilloCLI/builders/antlr-tool.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-export CLASSPATH=/home/maximus/vSphere/CabrilloCLI/antlr/antlr-4.4-complete.jar
-
+export CLASSPATH=./antlr/antlr-4.4-complete.jar
 java -Xmx500M org.antlr.v4.Tool "$@"
